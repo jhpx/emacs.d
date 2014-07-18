af934d5fbfd4a44cb1f05a125bf0d61733bf9368
jhpx <jhpx@hotmail.com>
2014-07-18 10:01:34 +0800

    Make custom.el and lisp/init-local.el under control.

---
+++ custom.el
@@ -0,0 +1,34 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(session-use-package t nil (session))
+
+ ;; Code Style Standard
+ '(flycheck-phpcs-standard "WordPress")
+ 
+ ;; Never backup in cureent directory
+ '(backup-directory-alist (quote (("." . "~/.backups"))))
+ 
+ ;; The color of "80 column rule" Guides line 
+ '(fci-rule-color "darkblue")
+ 
+ ;; For file encoding
+ '(revert-without-query (quote (".*")))
+ '(filename-coding-systems 'utf-8)
+ '(coding-system-for-write 'utf-8)
+
+ ; For tab and space
+ '(x-stretch-cursor t)
+ '(show-trailing-whitespace t)
+ )
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/init-local.el
@@ -0,0 +1,28 @@
+(add-hook 'prog-mode-hook 'sanityinc/prog-mode-fci-settings)
+(define-key global-map "\C-c\C-l" 'revert-buffer-with-gbk)
+(define-key global-map "\C-c\C-u" 'revert-buffer-with-utf8)
+(define-key global-map "\C-x\C-s" 'save-buffer-with-utf8)
+
+(defun save-buffer-with-utf8 ()
+  "Call `save-buffer' with utf8."
+  (interactive)
+  (let ((coding-system-for-write 'utf-8))
+    (save-buffer)))
+
+(defun revert-buffer-with-coding-system-no-confirm (coding-system)
+  "Call `revert-buffer-with-coding-system', but when `revert-buffer' do not need confirm."
+  (interactive "zCoding system for visited file (default nil): ")
+  (let ((coding-system-for-read coding-system))
+    (revert-buffer)))
+
+(defun revert-buffer-with-gbk ()
+  "Call `revert-buffer-with-coding-system-no-confirm' with gbk."
+  (interactive)
+  (revert-buffer-with-coding-system-no-confirm 'gbk))
+
+(defun revert-buffer-with-utf8 ()
+  "Call `revert-buffer-with-coding-system-no-confirm' with utf8."
+  (interactive)
+  (revert-buffer-with-coding-system-no-confirm 'utf-8))
+
+(provide 'init-local)
