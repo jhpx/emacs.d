ff7854cdf8489814b52d806207ae155ec1a24663
jhpx <jhpx@hotmail.com>
2015-08-04 15:09:26 +0800

    Add some file processing function about server-mode.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,13 +1,37 @@
-;; The color of "80 column rule" Guides line
+;;----------------------------------------------------------------------------;; The color of "80 column rule" Guides line
+;;----------------------------------------------------------------------------
 (add-hook 'prog-mode-hook 'sanityinc/prog-mode-fci-settings)
 
+;;----------------------------------------------------------------------------
 ;; Use Http proxy
+;;----------------------------------------------------------------------------
 (defconst proxy-server-string "127.0.0.1:8118")
 (setq url-proxy-services
  `(("http" . ,proxy-server-string)
  ("https" . ,proxy-server-string)))
 
+
+;;----------------------------------------------------------------------------
+;; For file processing
+;;----------------------------------------------------------------------------
+(define-key global-map "\C-x\C-c" 'kill-all-buffers-with-suspend)
+(define-key global-map "\C-xK" 'kill-all-buffers)
+
+(defun kill-all-buffers ()
+  "Kill all buffers and windows."
+  (interactive)
+  (mapcar 'kill-buffer (buffer-list))
+  (delete-other-windows))
+
+(defun kill-all-buffers-then-suspend ()
+  "Kill all buffers and windows then suspend."
+  (interactive)
+  (kill-all-buffers)
+  (sanityinc/maybe-suspend-frame))
+
+;;----------------------------------------------------------------------------
 ;; For file encoding
+;;----------------------------------------------------------------------------
 (define-key global-map "\C-c\C-l" 'revert-buffer-with-gbk)
 (define-key global-map "\C-c\C-u" 'revert-buffer-with-utf8)
 (define-key global-map "\C-x\C-s" 'save-buffer-with-utf8)
@@ -34,11 +58,15 @@
   (interactive)
   (revert-buffer-with-coding-system-no-confirm 'utf-8))
 
+;;----------------------------------------------------------------------------
 ;; Add support for some special languages
+;;----------------------------------------------------------------------------
 (require 'init-sml)
 (require 'init-racket)
 (require 'init-bison)
+;;----------------------------------------------------------------------------
 ;; Add some miscs
+;;----------------------------------------------------------------------------
 (require 'init-cjkfonts)
 
 (provide 'init-local)
